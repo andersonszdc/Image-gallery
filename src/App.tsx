3a5ad51b0fd0a5e8fac802d06f9201aee7372072
Anderson Souza <andersonszdc@gmail.com>
2022-03-26 22:18:06 -0300

    add overlay

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,52 @@ import "./App.css";
 import { motion } from "framer-motion";
 
 const items = [
-  { text: "1", position: { x: 0, y: 0 } },
-  { text: "2", position: { x: "calc(-50% - 8px)", y: 0 } },
-  { text: "3", position: { x: 0, y: "calc(-50% - 8px)" } },
-  { text: "4", position: { x: "calc(-50% - 8px)", y: "calc(-50% - 8px)" } },
+  { text: "1", position: { x: 0, y: 0 }, color: "#a03e99" },
+  {
+    text: "2",
+    position: { x: "calc(-50% - 8px)", y: 0 },
+    color: "#f59ca9",
+  },
+  {
+    text: "3",
+    position: { x: 0, y: "calc(-50% - 8px)" },
+    color: "#f6828c",
+  },
+  {
+    text: "4",
+    position: { x: "calc(-50% - 8px)", y: "calc(-50% - 8px)" },
+    color: "#df57bc",
+  },
 ];
 
 function App() {
   const [active, setActive] = useState<string>("");
+  const [last, setLast] = useState<string>("");
   return (
     <div className="App">
+      {active ? (
+        <motion.div
+          animate={{
+            backgroundColor: items[parseInt(active) - 1].color,
+            zIndex: 1,
+          }}
+          className="overlay"
+          transition={{ bounce: false, duration: 0.2 }}
+        />
+      ) : (
+        <motion.div
+          animate={{ backgroundColor: "#000" }}
+          className="overlay"
+          transition={{ bounce: false, duration: 0.2 }}
+        />
+      )}
       <ul className="gallery">
         {items.map((item, index) => (
           <Image
+            last={last}
+            setLast={setLast}
             setActive={setActive}
-            active={active === item.text}
+            active={active}
             item={item}
             key={index}
             position={item.position}
@@ -30,16 +61,22 @@ function App() {
 
 export default App;
 
-const Image = ({ item, active, setActive, position }: any) => {
+const Image = ({ item, active, setActive, position, setLast, last }: any) => {
   const handleClick = () => {
     setActive((prev: string) => (prev === item.text ? "" : item.text));
+    setLast(item.text);
   };
   return (
     <>
-      {!active ? (
+      {active !== item.text ? (
         <li onClick={handleClick} className="gallery__item">
           <motion.div
-            transition={{ bounce: false, duration: 0.2 }}
+            animate={{
+              width: "calc(50% - 8px)",
+              opacity: active ? 0 : 1,
+              zIndex: item.text === last ? 2 : 1,
+            }}
+            transition={{ bounce: false, duration: 0.4 }}
             className="image-container"
           >
             {item.text}
@@ -48,12 +85,13 @@ const Image = ({ item, active, setActive, position }: any) => {
       ) : (
         <li onClick={handleClick} className="gallery__item">
           <motion.div
-            transition={{ bounce: false, duration: 0.2 }}
+            transition={{ bounce: false, duration: 0.4 }}
             animate={{
               width: "100%",
               x: position.x,
               y: position.y,
-              zIndex: 1,
+              zIndex: 2,
+              color: item.color,
             }}
             className="image-container"
           >
