09e518879ef2669edf5096938d2a6fd684f0ade4
Anderson Souza <andersonszdc@gmail.com>
2022-03-26 21:13:21 -0300

    animation working

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 import { motion } from "framer-motion";
 
-const items = [{ text: "1" }, { text: "2" }, { text: "3" }, { text: "4" }];
+const items = [
+  { text: "1", position: { x: 0, y: 0 } },
+  { text: "2", position: { x: "calc(-50% - 8px)", y: 0 } },
+  { text: "3", position: { x: 0, y: "calc(-50% - 8px)" } },
+  { text: "4", position: { x: "calc(-50% - 8px)", y: "calc(-50% - 8px)" } },
+];
 
 function App() {
   const [active, setActive] = useState<string>("");
@@ -12,9 +17,10 @@ function App() {
         {items.map((item, index) => (
           <Image
             setActive={setActive}
-            active={active}
+            active={active === item.text}
             item={item}
             key={index}
+            position={item.position}
           />
         ))}
       </ul>
@@ -24,13 +30,37 @@ function App() {
 
 export default App;
 
-const Image = ({ item, active, setActive }: any) => {
+const Image = ({ item, active, setActive, position }: any) => {
   const handleClick = () => {
     setActive((prev: string) => (prev === item.text ? "" : item.text));
   };
   return (
-    <motion.li onClick={handleClick} className="gallery__item">
-      <div className="image-container">{item.text}</div>
-    </motion.li>
+    <>
+      {!active ? (
+        <li onClick={handleClick} className="gallery__item">
+          <motion.div
+            transition={{ bounce: false, duration: 0.2 }}
+            className="image-container"
+          >
+            {item.text}
+          </motion.div>
+        </li>
+      ) : (
+        <li onClick={handleClick} className="gallery__item">
+          <motion.div
+            transition={{ bounce: false, duration: 0.2 }}
+            animate={{
+              width: "100%",
+              x: position.x,
+              y: position.y,
+              zIndex: 1,
+            }}
+            className="image-container"
+          >
+            {item.text}
+          </motion.div>
+        </li>
+      )}
+    </>
   );
 };
